5756f0319aa6390dc6cc13ff3117c07775f96059
Coy Sanders <coymeetsworld@gmail.com>
2017-01-31 15:28:39 -0800

    Added footerbar with link to github repo and author name

--- app/app.jsx
+++ app/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Grid from 'Grid';
+import FooterBar from 'FooterBar';
 
 const App = () => (
 	<div>
 		<h1>Game of Life</h1>	
 		<Grid/>
+		<FooterBar/>
 	</div>
 )
 
